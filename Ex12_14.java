24a863acd62e2427f88ac2928d9b2df61f5344fd
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 22:46:14 +0800

    Add files via upload

---
+++ Ex12_14.java
@@ -0,0 +1,9 @@
+public class Ex12_14 {
+    public static void main(String[] args) {
+        String Q = "Habit is second nature";
+
+        String String = Q.substring(16, 22);
+
+        System.out.println(String);
+    }
+}
