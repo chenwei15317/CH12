7dac07029a57ed87e35e3eee80cfb9c979ba0b73
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 23:27:45 +0800

    Add files via upload

---
+++ Ex12_23.java
@@ -0,0 +1,11 @@
+public class Ex12_23 {
+    public static void main(String[] args) {
+
+        double N = 67.834;
+        String strN = String.valueOf(N);
+
+
+        System.out.println(strN);
+        System.out.println("字串長度:" + strN.length());
+    }
+}
