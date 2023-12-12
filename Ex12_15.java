5b13507c778678c91a764c52ae2af010ccd364c1
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 22:50:02 +0800

    Add files via upload

---
+++ Ex12_15.java
@@ -0,0 +1,10 @@
+public class Ex12_15 {
+    public static void main(String[] args) {
+        float Value = 123.45f;
+
+        String string= String.valueOf(Value);
+
+        System.out.println(string);
+        System.out.println("字串長度:" + string.length());
+    }
+}
