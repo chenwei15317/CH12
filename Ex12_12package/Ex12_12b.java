c976a2aab82511cace95e054e3530d95a6503c63
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 22:00:13 +0800

    Add files via upload

---
+++ Ex12_12package/Ex12_12b.java
@@ -0,0 +1,10 @@
+public class Ex12_12b {
+    public static void main(String[] args) {
+        
+        Sum s = new Sum();
+        System.out.println("Sum(5)=" + s.add(10));
+    }
+
+
+
+}
---
+++ Ex12_12package/Factor.java
@@ -0,0 +1,13 @@
+class Factor {
+
+    public int fac(int n) {
+
+        if (n >= 1)
+            return n * fac(n - 1);
+        else
+            return 1;
+    }
+
+
+    
+}
