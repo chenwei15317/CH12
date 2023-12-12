8c8cb8fe3215e3aa5286082d5bdc878cae82c59f
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 22:17:32 +0800

    Add files via upload

---
+++ Ex12_14.java
@@ -0,0 +1,9 @@
+public class Ex12_14 {
+    public static void main(String[] args) {
+        double Quest = 5.38;
+
+        long Ans = Math.round(Quest);
+
+        System.out.println("最接近" + Quest + "的整數是" + Ans);
+    }
+}
