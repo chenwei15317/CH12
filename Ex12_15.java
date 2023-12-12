52f7137f004230526f717376b54140cc31d803fe
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 22:44:18 +0800

    Delete Ex12_15.java

--- Ex12_15.java
+++
@@ -1,9 +0,0 @@
-public class Ex12_15 {
-    public static void main(String[] args) {
-        String Q = "Habit is second nature";
-
-        String String = Q.substring(16, 22);
-
-        System.out.println(String);
-    }
-}
