1b4bf390c6f21073d003ec0d301305368b0f95e9
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 22:52:37 +0800

    Add files via upload

---
+++ Ex12_16.java
@@ -0,0 +1,13 @@
+public class Ex12_16 {
+    public static void main(String[] args) {
+
+        String String = "262904713";
+
+        int intS = Integer.parseInt(String);
+
+        int FinalAns = intS * 2;
+
+        System.out.println(intS);
+        System.out.println("2倍的數值=" + FinalAns);
+    }
+}
