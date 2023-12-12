068f14cbfff0dee95624b23608ea0485f5d0d5e3
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 23:32:49 +0800

    Add files via upload

---
+++ Ex12_22.java
@@ -0,0 +1,11 @@
+public class Ex12_22 {
+    public static void main(String[] args) {
+
+        double sin = Math.PI / 6;
+        double cos = Math.PI / 3;
+        double Ans = Math.sin(sin) + Math.cos(cos);
+
+
+        System.out.println("sin(π/6) + cos(π/3) = " + Ans);
+    }
+}
