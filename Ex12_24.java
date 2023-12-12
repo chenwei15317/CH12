d05f514c65e7df72b26b4ee50017c2aea48fd412
chenwei15317 <145735022+chenwei15317@users.noreply.github.com>
2023-12-12 23:19:37 +0800

    Add files via upload

---
+++ Ex12_24.java
@@ -0,0 +1,9 @@
+public class Ex12_24 {
+    public static void main(String[] args) {
+        String Q = "Two heads are better than one";
+
+        String String = Q.substring(14, 20);
+
+        System.out.println(String);
+    }
+}
